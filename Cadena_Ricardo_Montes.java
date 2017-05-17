e1c2dece1902aa780b5c42d8cd63567ff28285d6
Ricardo_Montes_Lozano <Ricardo1984ML@gmail.com>
2017-05-17 19:18:05 +0200

    Aprendiendo

---
+++ Cadena_Ricardo_Montes.java
@@ -0,0 +1,31 @@
+
+package u05s03ex02;
+
+//Esto es un comentario de prueba
+
+public class Cadena {
+
+    public static void main(String[] args) {
+        String s = "hola";
+        System.out.println(s + ":");
+        s+="adios";
+        System.out.println(s + ":");
+        System.out.println(s.charAt(1));
+        
+//Esto es un comentario de prueba
+
+        String s1 = "Una cadena";
+        System.out.println(s1.length());
+        System.out.println(s1.charAt(4));
+        char [] v = new char [20];
+        s1.getChars(4, 6, v, 3);
+        
+        for (int i =0; i<10;i++){
+            System.out.println("v ["+i+"]= " + v[i]);
+        }
+ //Esto es un comentario de prueba
+       
+        String s2 = new String(v);
+    }
+    
+}
